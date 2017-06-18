e964bcbd675fec499a62d749db828e12286a08a4
0mg <millig@outlook.jp>
2017-06-18 11:59:45 +0900

    upd: F5 to reload & reset window pos

--- fulldraw.cpp
+++ fulldraw.cpp
@@ -406,6 +406,7 @@ LRESULT CALLBACK mainWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     switch (LOWORD(wp)) {
     case 0xAB32: {
       BOOL scs = wt.startMouseMode(hwnd) != NULL;
+      SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOSIZE);
       #ifdef dev
       wsprintf(ss, TEXT("fulldraw - %d, %d, %d"),
         wt.dll, wt.ctx, GetTickCount());
