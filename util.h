a1facc34edc97db1a0b234ddadd4272f2aec22ba
0mg <millig@outlook.jp>
2018-05-04 15:28:22 +0900

    dev: getError()

--- util.h
+++ util.h
@@ -1,5 +1,13 @@
 #include <windows.h>
 
+LPCTSTR getError() {
+  const SIZE_T len = 0x400;
+  TCHAR str[len];
+  FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, GetLastError(), 0,
+    str, len, NULL);
+  return str;
+}
+
 void popError(HWND hwnd = NULL) {
   const SIZE_T len = 0x400;
   TCHAR str[len];
