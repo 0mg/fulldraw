663a9579f80ca90603d0596c781b70e52134edf4
0mg <millig@outlook.jp>
2018-05-13 12:02:04 +0900

    fix: grab window up to foreground if not on started app

--- fulldraw.cpp
+++ fulldraw.cpp
@@ -246,6 +246,8 @@ LRESULT CALLBACK mainWndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
     return 0;
   }
   case WM_ERASEBKGND: {
+    // window.onload: set window to top most if not.
+    SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, C_SCWIDTH, C_SCHEIGHT, 0);
     return 1;
   }
   case WM_PAINT: {
